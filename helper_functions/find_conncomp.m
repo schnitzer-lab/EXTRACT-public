c4dbc614540eb24d4900c3e89c230821af529956
Fatih Dinc <56279691+fatihdinc@users.noreply.github.com>
2021-03-11 00:17:53 +0300

    Replace find_conncomp.m

--- helper_functions/find_conncomp.m
+++ helper_functions/find_conncomp.m
@@ -1,16 +1,19 @@
-function [cc, A] = find_conncomp(X, threshold, A2)    
+function [cc, A] = find_conncomp(X, threshold, A2)
 % Finds connected components of columns of A wrt a corr. threshold
 % A2 (optional) is an additional adjacency matrix as a multiplicative constraint
 
-    mean_X = mean(X, 1);
-    X_norm = bsxfun(@minus, X, mean_X);
-    norms_X = sqrt(sum(X_norm.^2, 1));
-    X_norm = bsxfun(@rdivide, X_norm, norms_X + 1e-6);
-    A = gather((X_norm' * X_norm) > threshold);
+    tiny = 1e-6;
+    XpX = full(X'*X);
+    mean_X = full(mean(X, 1));
+    XpX_norm = XpX - size(X, 1)*(mean_X'*mean_X);
+    norms_X = sqrt(diag(XpX_norm)') + tiny;
+    C = XpX_norm ./ (norms_X'*norms_X);
+    A = gather(C > threshold);
+
     if exist('A2', 'var')
         A = A .* A2;
     end
-    [s, memberships] = graphconncomp(sparse(A)); 
+    [s, memberships] = graphconncomp(sparse(A));
     cc = [];
     acc=0;
     for k = 1:s
@@ -22,6 +25,6 @@ function [cc, A] = find_conncomp(X, threshold, A2)
             % idx with most common neighbors goes last
             cc(acc).indices = [idx(setdiff(1:length(idx), i)), idx(i)];
         end
-    end       
-    
+    end
+
 end
