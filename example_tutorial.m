3efe7c38e5ffc54e49db5d4fc6ccef275b005561
Fatih Dinc <56279691+fatihdinc@users.noreply.github.com>
2021-03-21 01:48:44 +0300

    Update example_tutorial.m

--- example_tutorial.m
+++ example_tutorial.m
@@ -15,13 +15,13 @@ config.avg_cell_radius=7; %Average cell radius is 7.
 config.trace_output_option='raw'; % Choose 'nonneg' for non-negative Ca2+ traces, 'raw' for raw ones!
 config.num_partitions_x=1;
 config.num_partitions_y=1; 
-config.cellfind_filter_type='none'; % The movie is clean enough, no need for filtering
+config.cellfind_filter_type='none'; % The movie is clean enough, no need for lowpass filtering
 config.verbose=2; %Keeping verbose=2 gives insight into the EXTRACTion process, always advised to keep 2
-
+config.spatial_highpass_cutoff=inf % no need for highpass filtering
+config.remove_stationary_background=0 %no need for background removal
 
 % Optionals whose defaults exist:
 config.use_gpu=0; % This is a small dataset, will be fast on cpu anyways.
-config.preprocess=0; %The movie is already preprocessed.
 config.max_iter = 10; % 10 is a good number for this dataset
 config.adaptive_kappa = 1;% Adaptive kappa is on for this movie. For an actual movie, keeping it off
 % may be beneficial depending on the noise levels.
