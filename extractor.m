dc6c76adb8827805d010a4bfb6c87baf1b1c4156
Fatih Dinc <56279691+fatihdinc@users.noreply.github.com>
2021-03-21 00:38:46 +0300

    Update extractor.m

--- extractor.m
+++ extractor.m
@@ -257,7 +257,7 @@ end
 end_time = posixtime(datetime);
 total_runtime = end_time - start_time - io_time;
 
-info.version = '0.6.1';
+info.version = '0.7.0';
 info.summary = summary;
 info.runtime = total_runtime;
 info.summary_image = summary_image;
