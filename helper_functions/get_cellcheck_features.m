bec6cb580ced0ffb56b4e4e227484c72df263a10
Fatih Dinc <56279691+fatihdinc@users.noreply.github.com>
2021-07-02 02:54:52 +0300

    Update get_cellcheck_features.m

--- helper_functions/get_cellcheck_features.m
+++ helper_functions/get_cellcheck_features.m
@@ -2,6 +2,6 @@ function [is_attr_bad,metrics,is_elim] = get_cellcheck_features(output)
     
 is_attr_bad=output.info.cellcheck.is_attr_bad;
 metrics=output.info.cellcheck.metrics;
-is_elim=output.info.cellcheck.is_bad;
+is_elim=logical(output.info.cellcheck.is_bad);
 
 end
