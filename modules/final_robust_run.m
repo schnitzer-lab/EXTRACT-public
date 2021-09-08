b5baa0d6e65f111a231ad67ad9b5b7d40cb60dd4
fatihdinc <fdinc@stanford.edu>
2021-09-08 08:42:44 -0700

    Update final_robust_run.m

--- modules/final_robust_run.m
+++ modules/final_robust_run.m
@@ -1,7 +1,10 @@
-function [traces]=final_robust_run(output_in, M)
+function [traces,filters]=final_robust_run(M,output_in,choices)
 	
 	% This function runs a final robust regression on the movie via the sorted cell filters. Note that this function is not optimized for memory consumption,
 	% so if you need to process in chunks, please run an outside for loop and chunk the movie only across time axis, no partitioning in space is allowed!
+
+	% Inputs: M is the movie, output_in is the EXTRACT output from the last run of EXTRACT and choices is an array of 1s and 0s denoting cell
+	% sorting output. Note that output_in should not be changed during cell-sorting, only choices array is created!
 
 	config=output_in.config;
 
@@ -16,9 +19,13 @@ function [traces]=final_robust_run(output_in, M)
 	config.max_iter=0;
 
 
-	S_in=full(output_in.spatial_weights);
+
+	filters=full(output.spatial_weights(:,:,logical(choices)));
+	traces_in=output.temporal_weights(:,logical(choices));
+
+	S_in=filters;
 	config.S_init=reshape(S_in, size(S_in, 1) * size(S_in, 2), size(S_in, 3));
-	config.T_init=output_in.temporal_weights';
+	config.T_init=traces_in';
 
 	output=extractor(M,config);
 
