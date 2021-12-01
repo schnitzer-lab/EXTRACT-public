c20d277c01307f888467911a26576641ab7405f0
Fatih Dinc <56279691+fatihdinc@users.noreply.github.com>
2021-11-30 18:21:14 -0800

    Update get_defaults.m

--- get_defaults.m
+++ get_defaults.m
@@ -3,7 +3,7 @@ function config = get_defaults(config)
     % General control parameters 
 
     if ~isfield(config, 'trace_output_option'), config.trace_output_option = 'robust'; end
-    if ~isfield(config, 'trace_quantile'), config.trace_quantile = 0.1; end
+    if ~isfield(config, 'trace_quantile'), config.trace_quantile = 0.25; end
     if ~isfield(config, 'use_gpu'), config.use_gpu = true; end
     if ~isfield(config, 'parallel_cpu'), config.parallel_cpu = false; end
     if ~isfield(config, 'dendrite_aware'), config.dendrite_aware = false; end
