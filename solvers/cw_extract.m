c77dd708763ea81d7d98f604f468873e31857845
fatihdinc <fdinc@stanford.edu>
2021-12-20 21:59:51 +0300

    Update cw_extract.m

--- solvers/cw_extract.m
+++ solvers/cw_extract.m
@@ -76,10 +76,12 @@ if config.visualize_cellfinding
     subplot(222)
     histogram(trace_snr_all)
     xlabel('Trace snr')
+    ylabel('Number of good cells')
     drawnow;
     subplot(224)
     histogram(mov_snr_all)
     xlabel('cellfind min snr')
+    ylabel('Number of good cells')
     drawnow;
     
 end
