db2ebbcc9ab5bd08768c47c1c0b269dc7ddab3b4
fatihdinc <fdinc@stanford.edu>
2022-01-03 16:11:13 +0300

    scripts

---
+++ Template scripts/downsample_and_run_extract.m
@@ -0,0 +1,52 @@
+%% Start the main pipeline
+
+movie_info = h5info('example.h5','/Data');
+movie_size = num2cell(movie_info.Dataspace.Size);
+[nx, ny, totalnum] = deal(movie_size{:});
+
+%% Downsample the movie
+downsampletime_pipeline('example.h5:/Data',40,4,40000)
+% Downsamples the first 40000 frames of the movie by 4 using 40 blocks. You can downsample the movie down to 2Hz, maybe even more...
+
+
+%% run EXTRACT on the downsampled movie
+
+M = h5read('example_downsampled.h5','/Data');
+config =[]
+config = get_defaults(config);
+
+config.avg_cell_radius=6;
+config.num_partitions_x=1;
+config.num_partitions_y=1;
+
+% change these as needed
+config.cellfind_min_snr = 1;
+config.thresholds.T_min_snr=10;
+
+output=extractor(M,config);
+save('extract_downsampled_unsorted.mat','output','-v7.3');
+
+% While it is optional, it is beneficial to sort the cells at this stage before moving forward.cellfind_min_snr
+
+%% run EXTRACT on the full movie 
+load('unsorted_extract.mat');
+M = 'example.h5:/Data';
+config = output.config;
+
+config.avg_cell_radius=6;
+
+% Add more partitions as needed for the RAM memory. As a rule of thumb, you want to partition the movie such that partitioned movie memory is 1/4th of RAM memory.
+config.num_partitions_x=2;
+config.num_partitions_y=2;
+
+config.max_iter=0;
+
+% If you sorted, make sure that S_in is the sorted cell filters!
+S_in=output.spatial_weights;
+config.S_init=full(reshape(S_in, size(S_in, 1) * size(S_in, 2), size(S_in, 3)));
+
+output=extractor(M,config);
+save('extrat_full_sorted.mat','output','-v7.3');
+
+
+
---
+++ Template scripts/motion_correction_denoising_script.m
@@ -0,0 +1,37 @@
+%% Start the main pipeline
+
+
+movie_info = h5info('example.h5','/Data');
+movie_size = num2cell(movie_info.Dataspace.Size);
+[nx, ny, totalnum] = deal(movie_size{:});
+
+
+%% Downsample in space
+
+% If the cells are relatively large (say larger than 6 pixel radius), you can downsample in space to decrease runtimes later on. Otherwise, skip!
+downsamplespace_pipeline('example.h5:/Data',40,2,'h5',40000);
+% Downsamples in space the first 40000 frames of the input movie (in h5 format) by a factor of 2, in 40 chunks.
+
+%% Run motion correction
+
+config_mc =[];
+config_mc = get_defaults_mc(config_mc);
+
+% By default, nonrigid correction is off. You can turn it on like this
+config.nonrigid_mc=1;
+run_normcorre_svd('example.h5:/Data','example_mc.h5:/Data',config_mc);
+%% Denoise the movie
+
+% if the movie is too noisy, you can consider denoising it!
+%denoisingSVDh5('example_mc.h5:/Data');
+
+%% Downsample the original or the denoised movie
+downsampletime_pipeline('example_mc.h5:/Data',40,4,40000)
+% Downsamples the first 40000 frames of the movie by 4 using 40 blocks. You can downsample the movie down to 2Hz, maybe even more...
+
+% This concludes the preparation of the movie for cell extraction
+
+
+
+
+
