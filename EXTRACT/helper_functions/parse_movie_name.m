a2ff81d14c29d5e9bb6adc180be6cd5872e3e2c1
fatihdinc <fdinc@stanford.edu>
2022-02-10 15:49:03 -0800

    Update parse_movie_name.m

--- EXTRACT/helper_functions/parse_movie_name.m
+++ EXTRACT/helper_functions/parse_movie_name.m
@@ -1,7 +1,17 @@
 function [path, dataset] = parse_movie_name(M)
     str_cell = strsplit(M, ':');
-    if length(str_cell) ~= 2
+    if length(str_cell) == 2
+        [path, dataset] = deal(str_cell{:});
+    elseif length(str_cell) > 2
+        s = length(str_cell);
+        path = [];
+        for i = 1:s-1
+            path = [path str_cell{i} ':'];
+        end
+        path = path(1:end-1);
+    
+        dataset = str_cell{end};
+    else
         error('Movie string must be in the format "filepath:dataset"');
     end
-    [path, dataset] = deal(str_cell{:});
 end
