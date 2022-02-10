3dc765e19cd99d6af7a9557e06bc4712db6349db
fatihdinc <fdinc@stanford.edu>
2022-02-10 15:56:26 -0800

    motion_correct_changes

--- External algorithms/run_normcorre_pipeline.m
+++ External algorithms/run_normcorre_pipeline.m
@@ -9,7 +9,7 @@ bandpass        = config.bandpass;
 avg_cell_radius = config.avg_cell_radius;
 use_gpu         = config.use_gpu;
 file_type       = config.file_type;
-
+mask            = config.mask;
 
 switch file_type
     case 'h5'
@@ -80,17 +80,39 @@ if isempty(template)
         case 'tiff'
             im1 = single(read_from_tif(input,1,nt_template));
     end
+    
+    im1_nonrig = im1;
+    
+    if ~isempty(mask)
+        idx_2 = find(sum(mask,1)==0);
+        idx_1 = find(sum(mask,2)==0);
+        im1(idx_1(:),:,:)=[];
+    end
+    
 
     if bandpass
         im1 = spatial_bandpass(im1,avg_cell_radius,10,2,use_gpu);
+        im1_nonrig = spatial_bandpass(im1_nonrig,avg_cell_radius,10,2,use_gpu);
     end
 
 
     %im_ds= single(max(im1,[],3));
     im_ds= single(mean(im1,3));
+    im_ds_nonrig = single(mean(im1_nonrig,3));
+    figure
+    imshow(im_ds,[]);
+    drawnow;
     clear im1
+
 else
     im_ds = single(template);
+    im_ds_nonrig = im_ds;
+    if ~isempty(mask)
+        idx_2 = find(sum(mask,1)==0);
+        idx_1 = find(sum(mask,2)==0);
+        im_ds(idx_1(:),:)=[];
+        im_ds(:,idx_2(:))=[];
+    end
 end
 
 disp(sprintf('%s: Running motion correction split into %s movies', datestr(now),num2str(numel(startno)) ))
@@ -111,14 +133,18 @@ for i=1:numel(startno)
             M = single(read_from_tif(input,startno(i),perframes(i)));
     end
     
-    
+    M_proc = M;
+    if ~isempty(mask)
+        M_proc(idx_1(:),:,:)=[];
+        M_proc(:,idx_2(:),:)=[];
+    end
     
 
     if bandpass
-        M_proc = spatial_bandpass(M,avg_cell_radius,10,2,use_gpu);
-    else
-        M_proc = M;
+        M_proc = spatial_bandpass(M_proc,avg_cell_radius,10,2,use_gpu);
     end
+    
+
     
 
     
@@ -126,15 +152,19 @@ for i=1:numel(startno)
     
     disp(sprintf('\t \t %s: Starting rigid motion correction ', datestr(now)))
 
-    options_rigid = NoRMCorreSetParms('d1',nx,'d2',ny,'max_shift',30,'us_fac',30,'grid_size',[nx,ny],'print_msg',0); 
+    options_rigid = NoRMCorreSetParms('d1',size(M_proc,1),'d2',size(M_proc,2),'max_shift',30,'us_fac',30,'grid_size',[size(M_proc,1),size(M_proc,2)],'print_msg',0); 
     % The grid size is full FOV
     [~,shifts_rigid,~,options_rigid] = normcorre_batch(M_proc,options_rigid,im_ds);
 
     
     clear M_proc
     
-
-
+    if ~isempty(mask)
+        options_rigid.d1=nx;
+        options_rigid.d2=ny;
+        options_rigid.grid_size = [nx,ny,1];
+    end
+    
     M_rigid = zeros([nx, ny, perframes(i)],'single');
     M_rigid = apply_shifts(M,shifts_rigid,options_rigid);
 
@@ -147,19 +177,19 @@ for i=1:numel(startno)
 
         disp(sprintf('\t \t %s: Starting non-rigid motion correction ', datestr(now)))
 
-
+        M_proc = M_rigid;
+        
 
         if bandpass
-            M_proc = spatial_bandpass(M_rigid,avg_cell_radius,10,2,use_gpu);
-        else
-            M_proc = M_rigid;
+            M_proc = spatial_bandpass(M_proc,avg_cell_radius,10,2,use_gpu);
         end
+        
 
         
         
         
-        options_nonrigid = NoRMCorreSetParms('d1',nx,'d2',ny,'max_shift',30,'us_fac',30,'grid_size',[ns_nonrigid,ns_nonrigid],'print_msg',0);
-        [~,shifts_nonrigid,~,options_nonrigid] = normcorre_batch(M_proc,options_nonrigid,im_ds); 
+        options_nonrigid = NoRMCorreSetParms('d1',nx,'d2',ny,'max_shift',round(ns_nonrigid/5),'us_fac',30,'grid_size',[ns_nonrigid,ns_nonrigid],'print_msg',0);
+        [~,shifts_nonrigid,~,options_nonrigid] = normcorre_batch(M_proc,options_nonrigid,im_ds_nonrig); 
 
         
         clear M_proc
