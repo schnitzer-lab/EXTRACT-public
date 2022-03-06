b9f400149d4293240df49666f4a8f61b9d32eb66
fatihdinc <fdinc@stanford.edu>
2022-03-06 14:56:39 -0800

    Update get_defaults_mc.m

--- External algorithms/get_defaults_mc.m
+++ External algorithms/get_defaults_mc.m
@@ -1,6 +1,6 @@
 function config = get_defaults_mc(config)
 
-    if ~isfield(config, 'nt_template'), config.nt_template = 1000; end
+    if ~isfield(config, 'nt_template'), config.nt_template = 100; end
     if ~isfield(config, 'template'), config.template = []; end
     if ~isfield(config, 'numFrame'), config.numFrame = 1000; end
     if ~isfield(config, 'nonrigid_mc'), config.nonrigid_mc = 0; end
@@ -10,4 +10,5 @@ function config = get_defaults_mc(config)
     if ~isfield(config, 'use_gpu'), config.use_gpu = 0; end
     if ~isfield(config, 'file_type'), config.file_type = 'h5'; end
     if ~isfield(config, 'mask'), config.mask = []; end
+    if ~isfield(config, 'mc_template'), config.mc_template = 0; end
 end
