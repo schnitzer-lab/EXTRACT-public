ebed7e8bf0a0e4e823d386f79ee8bd07caca83e3
fatihdinc <fdinc@stanford.edu>
2022-03-16 14:48:03 -0700

    Update extractor.m

--- EXTRACT/solvers/extractor.m
+++ EXTRACT/solvers/extractor.m
@@ -433,7 +433,7 @@ end
 end_time = posixtime(datetime);
 total_runtime = end_time - start_time - io_time;
 
-info.version = '1.0.0';
+info.version = '1.0.1';
 info.summary = summary;
 info.runtime = total_runtime;
 info.summary_image = summary_image;
