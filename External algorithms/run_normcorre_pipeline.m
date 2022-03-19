bf93aeac704b023984de1c6619b898d693bf6c30
fatihdinc <fdinc@stanford.edu>
2022-03-18 22:00:41 -0700

    Update run_normcorre_pipeline.m

--- External algorithms/run_normcorre_pipeline.m
+++ External algorithms/run_normcorre_pipeline.m
@@ -93,10 +93,10 @@ if isempty(template)
         figure
         imshow(mean(im1,3),[])
         rect = getrect
-        y_min = round(rect(1))
-        x_min = round(rect(2))
-        y_max = round(rect(1)+rect(3))
-        x_max = round(rect(2)+rect(4))
+        y_min = round(rect(1));
+        x_min = round(rect(2));
+        y_max = round(rect(1)+rect(3));
+        x_max = round(rect(2)+rect(4));
         mask = zeros(nx,ny);
         mask(x_min:x_max,y_min:y_max) = 1;
     end
