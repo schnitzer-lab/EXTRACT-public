3430435b6abe5716017de3bb92a1d2057c81a57a
fatihdinc <fdinc@stanford.edu>
2022-03-26 14:20:22 -0700

    Update plot_hyperparameter_curves.m

--- EXTRACT/debug_utils/plot_hyperparameter_curves.m
+++ EXTRACT/debug_utils/plot_hyperparameter_curves.m
@@ -6,10 +6,9 @@ function [] = plot_hyperparameter_curves(output)
     figure
     subplot(2,3,1)
     metric      = metrics(fmap('T_maxval'), :);
-    metric_thrs = thresholds.T_min_snr;
     edges = [0:3:100];
     histogram(metric, edges);
-    xline(metric_thrs,'--r','LineWidth',4);
+    xline(10,'--r','LineWidth',4);
     xlabel('T min snr');
 
 
@@ -17,32 +16,28 @@ function [] = plot_hyperparameter_curves(output)
     metric      = metrics(fmap('S_corruption'), :);
     edges = [min(metric):(max(metric)-min(metric))/30:max(metric)];
     histogram(metric, edges);
-    metric_thrs = thresholds.spatial_corrupt_thresh;
-    xline(metric_thrs,'--g','LineWidth',4);
+    xline(0.7,'--g','LineWidth',4);
     xlabel('Spatial corrupt thresh');
 
     subplot(2,3,3)
     metric      = metrics(fmap('ST2_index_3'), :);
     edges = [min(metric):(max(metric)-min(metric))/30:max(metric)];
     histogram(metric, edges);
-    metric_thrs = thresholds.low_ST_index_thresh;
-    xline(metric_thrs,'--r','LineWidth',4);
+    xline(0.01,'--r','LineWidth',4);
     xlabel('low ST index thresh');
     
     subplot(2,3,4)
     metric      = metrics(fmap('S_eccent'), :);
     edges = [min(metric):(max(metric)-min(metric))/30:max(metric)];
     histogram(metric, edges);
-    metric_thrs = thresholds.eccent_thresh;
-    xline(metric_thrs,'--g','LineWidth',4);
+    xline(6,'--g','LineWidth',4);
     xlabel('Eccent thresh');
     
     subplot(2,3,5)
     metric      = metrics(fmap('T_corruption'), :);
-    metric_thrs = thresholds.temporal_corrupt_thresh;
     edges = [0:.03:1];
     histogram(metric, edges);
-    xline(metric_thrs,'--g','LineWidth',4);
+    xline(0.7,'--g','LineWidth',4);
     xlabel('Temporal corrupt thresh');
 
 
@@ -51,12 +46,7 @@ function [] = plot_hyperparameter_curves(output)
     metric = metric/avg_cell_area;
     edges = [min(metric):(max(metric)-min(metric))/30:max(metric)];
     histogram(metric, edges);
-    metric_thrs = thresholds.size_lower_limit;
-    xline(metric_thrs,'--r','LineWidth',4);
-    metric_thrs = thresholds.size_upper_limit;
-    xline(metric_thrs,'--g','LineWidth',4);
     xlabel('Lower and Upper Size limits');
-    set(gca,'Xscale','log');
 
 end
 
