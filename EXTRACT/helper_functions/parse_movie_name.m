a436e5546aa785cd6cef0933509acfe4a37c0feb
fatihdinc <fdinc@stanford.edu>
2022-04-07 15:24:48 -0700

    parse_movie_name changes

--- EXTRACT/helper_functions/parse_movie_name.m
+++ EXTRACT/helper_functions/parse_movie_name.m
@@ -1,17 +1,23 @@
 function [path, dataset] = parse_movie_name(M)
-    str_cell = strsplit(M, ':');
-    if length(str_cell) == 2
-        [path, dataset] = deal(str_cell{:});
-    elseif length(str_cell) > 2
-        s = length(str_cell);
-        path = [];
-        for i = 1:s-1
-            path = [path str_cell{i} ':'];
-        end
-        path = path(1:end-1);
-    
-        dataset = str_cell{end};
+
+    if iscell(M)
+        path = M{1};
+        dataset = M{2};
     else
-        error('Movie string must be in the format "filepath:dataset"');
+        str_cell = strsplit(M, ':');
+        if length(str_cell) == 2
+            [path, dataset] = deal(str_cell{:});
+        elseif length(str_cell) > 2
+            s = length(str_cell);
+            path = [];
+            for i = 1:s-1
+                path = [path str_cell{i} ':'];
+            end
+            path = path(1:end-1);
+        
+            dataset = str_cell{end};
+        else
+            error('Movie string must be in the format "filepath:dataset"');
+        end
     end
 end
