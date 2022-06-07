9f10d6c2fc9678ba49de1b85c4375135d662e6a8
Fatih Dinc <fdinc@stanford.edu>
2022-06-06 20:35:43 -0700

    Create get_arbitrary_mask.m

---
+++ EXTRACT/helper_functions/get_arbitrary_mask.m
@@ -0,0 +1,16 @@
+function mask = get_arbitrary_mask(M)
+    if ischar(M) || iscell(M)
+        [path, dataset] = parse_movie_name(M);
+        [h, w, t] = get_movie_size(M);
+        M_out = h5read(path, dataset, [1,1,1], [h,w,min(100,t)]);
+        im = max(M_out, [], 3);
+        clear M_out
+    else
+        im = max(M, [], 3);
+    end
+    
+    c_lim = quantile(im(:),[0 0.9])
+    imshow(im,c_lim)
+    mask = createMask(roi)
+
+end
