553d814e9711abd1931916f47762527b967f5183
Fatih Dinc <fdinc@stanford.edu>
2022-07-22 18:04:30 -0700

    Update get_defaults.m

--- EXTRACT/get_defaults.m
+++ EXTRACT/get_defaults.m
@@ -41,6 +41,7 @@ function config = get_defaults(config)
     if ~isfield(config, 'cellfind_min_snr'), config.cellfind_min_snr = 1; end
     if ~isfield(config, 'cellfind_max_steps'), config.cellfind_max_steps = 1000; end
     if ~isfield(config, 'cellfind_kappa_std_ratio'), config.cellfind_kappa_std_ratio = 0.7; end
+    if ~isfield(config, 'cellfind_adaptive_kappa'), config.cellfind_adaptive_kappa = 1; end
     if ~isfield(config, 'init_with_gaussian'), config.init_with_gaussian = false; end
     if ~isfield(config, 'avg_yield_threshold'), config.avg_yield_threshold = 0.1; end
 
