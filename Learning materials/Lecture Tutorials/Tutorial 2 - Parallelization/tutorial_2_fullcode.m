77f19d3545a43f30b58971fac69d79ebb01afd47
fatihdinc <fdinc@stanford.edu>
2024-07-03 23:11:52 -0700

    tutorials 1-3 done

--- Learning materials/Lecture Tutorials/Tutorial 2 - Parallelization/tutorial_2_fullcode.m
+++ Learning materials/Lecture Tutorials/Tutorial 2 - Parallelization/tutorial_2_fullcode.m
@@ -1,6 +1,6 @@
 %% Create the movie
-%clear
-%clc
+clear
+clc
 if ~isfile('Example_2p_movie.h5')
     [opts_2p] = get_2p_defaults();
     opts_2p.ns = 500;
@@ -10,21 +10,16 @@ end
 %% Run EXTRACT
 M = 'Example_2p_movie.h5:/mov';
 config = get_defaults([]);
-
-% Other tutorials will explain how to tune these parameters.
 config.adaptive_kappa = 2;
 config.spatial_highpass_cutoff = inf;
 config.downsample_time_by = 4;
 config.num_partitions_x = 4;
 config.num_partitions_y = 4;
-config.thresholds.S_dup_corr_thresh = 0.8;
 config.thresholds.T_dup_corr_thresh = 0.99;
 config.thresholds.spatial_corrupt_thresh = 0.1;
 config.thresholds.eccent_thresh = 3;
 config.max_iter = 10;
 config.thresholds.size_upper_limit = 3;
-config.thresholds.size_lower_limit = .1;
-config.visualize_cellfinding = 0;
 config.cellfind_min_snr = 0;
 config.thresholds.T_min_snr = 7;
 config.verbose = 2;
